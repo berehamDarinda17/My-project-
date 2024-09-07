ee7cc6815503aaabc0e7c1740b11292ab4cd17b8
berehadDarinda17 <Omsrivastav1710@gmail.com>
2024-09-07 11:06:24 +0530

    first commit

---
+++ ankur/src/App.js
@@ -0,0 +1,27 @@
+// import logo from './logo.svg';
+// import './App.css';
+// // alert("Welcome to My Website in this page i try to do something unique")
+
+// function App() {
+//   return (
+//     <div>
+//       <header>Ragistration Form</header>
+//         <Form>
+//           <label htmlFor="name">Name</label>
+//           <input type="text" required></input><br></br>
+//           <label htmlFor="lname">Last Name</label>
+//           <input type="text" required></input>input<br></br>
+//           <label htmlFor="name">Telephone No.</label>
+//           <input type="number" maxlength="10" minlength="10" required></input><br>
+//           <label htmlFor="email">E-mail</label>
+//           <input type="e-mail"><br></br>
+//           <label htmlFor="CP">Create Password</label>
+//           <input type="password" required><br></br>
+//           <button type="submit"> Submit </button><br></br>
+//           <p>Don't have an account</p><a href="#"> Click Here </a>
+//         </Form>
+//     </div>
+//   );
+// }
+
+// export default App;
